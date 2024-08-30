717867d8a94058b95ef3bf926cfb8dd212c2285a
Irtaza Ahmed <irtazaahmedk@gmail.com>
2024-08-30 21:03:33 +0500

    assignment 1 complete

---
+++ slide/app.js
@@ -0,0 +1,11 @@
+let firstName;
+let lastName;
+firstName = "irtaza"
+lastName = "ahmed"
+
+let address = "main street"
+address = "first street"
+
+console.log(firstName)
+console.log(lastName)
+console.log(address)
